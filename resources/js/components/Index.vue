1024afb43d5b37252845659e0fb778f1eff8f32b
Viktor <meitlin@gmail.com>
2019-02-06 22:45:02 +0200

    fix

--- resources/js/components/Index.vue
+++ resources/js/components/Index.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>hello</div>
+  <div class="container">
+    <br><br>
+    <h1 class="header center orange-text">Учет затрат</h1>
+    <div class="row center">
+      <h5 class="header col s12 light">Учет любых затрат или прибыли</h5>
+    </div>
+    <div class="row center">
+      <h5 class="col s12">Начало работы</h5>
+      <p>Для начала работы необходимо зарегистрироваться, после этого нужно создать категории и можно вносить затраты</p>
+    </div>
+    <br><br>
+
+  </div>
 </template>
 
 <script>
